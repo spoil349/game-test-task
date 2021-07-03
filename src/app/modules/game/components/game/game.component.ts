06224fa43981b743ece0deaf7389b24ca683f5ab
spoil349 <spoil349@mail.ru>
2021-07-03 22:48:53 +0400

    feat: add HostListener(on key up) in game component, add keyEventCode, colorMap object

--- src/app/modules/game/components/game/game.component.ts
+++ src/app/modules/game/components/game/game.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Item } from '../../models/item';
 
 @Component({
   selector: 'app-game',
@@ -7,9 +8,118 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
+  keyEventCodeMap: {[type: string]: string} = {
+    ArrowRight: 'ArrowRight',
+    ArrowLeft: 'ArrowLeft',
+    ArrowUp: 'ArrowUp',
+    ArrowDown: 'ArrowDown',
+  }
+
+  colorMap: {[k:number]: string} = {
+    2: '#626567',
+    4: '#424949',
+    8: '#7E5109',
+    16: '#196F3D',
+    32: '#138D75',
+    64: '#154360',
+    128: '#9B59B6',
+    256: '#78281F',
+    512: '#C0392B',
+    1024: '#7D6608',
+    2048: '#45B39D'
+  };
+
+  items: Item[] = [
+    {
+      value: 2,
+      col: 1,
+      row: 1,
+    },
+
+    {
+      value: 4,
+      col: 3,
+      row: 1,
+    },
+
+    {
+      value: 8,
+      col: 1,
+      row: 3,
+    },
+
+    {
+      value: 16,
+      col: 2,
+      row: 2,
+    },
+
+    {
+      value: 32,
+      col: 2,
+      row: 3,
+    },
+
+    {
+      value: 64,
+      col: 4,
+      row: 1,
+    },
+
+    {
+      value: 128,
+      col: 4,
+      row: 2,
+    },
+
+    {
+      value: 256,
+      col: 4,
+      row: 3,
+    },
+
+    {
+      value: 512,
+      col: 4,
+      row: 4,
+    },
+
+    {
+      value: 1024,
+      col: 2,
+      row: 1,
+    },
+
+    {
+      value: 2048,
+      col: 2,
+      row: 4 ,
+    },
+
+    
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getStyles(item: Item): {[p: string]: string} {
+    const top = (item.row * 110 - 100) + 'px';
+    const left = (item.col * 110 - 100) + 'px';
+    return {
+      top,
+      left,
+      'background-color': this.colorMap[item.value] || 'black'
+    };
+  }
+
+  @HostListener('window:keyup', ['$event'])
+
+  onKeyUp(event: KeyboardEvent) {
+    if (this.keyEventCodeMap[event.code]) {
+      console.log(this.keyEventCodeMap[event.code])
+    }
   }
 
 }
